1ed389532d5d7274b92b798c4c26733ec62a996f
wyatt <wcopping@asu.edu>
2018-07-29 12:45:54 -0700

    add/set up framebuffer, from ch 17 'Framebuffers'

--- vulkan-triangle/main.cpp
+++ vulkan-triangle/main.cpp
@@ -99,6 +99,7 @@ private:
   VkRenderPass render_pass;
   VkPipelineLayout pipeline_layout;
   VkPipeline graphics_pipeline;
+  std::vector<VkFramebuffer> swap_chain_framebuffers;
 
 
   void init_vulkan() {
@@ -111,6 +112,7 @@ private:
     create_image_views();
     create_render_pass();
     create_graphics_pipeline();
+    create_framebuffers();
   }
 
   void main_loop() {
@@ -266,6 +268,9 @@ private:
 
 
   void cleanup() {
+    for (auto framebuffer : swap_chain_framebuffers) {
+      vkDestroyFramebuffer(device, framebuffer, nullptr);
+    }
     vkDestroyPipeline(device, graphics_pipeline, nullptr);
     vkDestroyPipelineLayout(device, pipeline_layout, nullptr);
     vkDestroyRenderPass(device, render_pass, nullptr);
@@ -1007,6 +1012,28 @@ private:
     render_pass_info.pSubpasses = &subpass;
     if (vkCreateRenderPass(device, &render_pass_info, nullptr, &render_pass) != VK_SUCCESS) {
       throw std::runtime_error("failed to create render pass!");
+    }
+  }
+
+  void create_framebuffers() {
+    swap_chain_framebuffers.resize(swap_chain_image_views.size());
+    for (size_t i; i < swap_chain_image_views.size(); i++) {
+      VkImageView attachments[] = { swap_chain_image_views[i] };
+
+      VkFramebufferCreateInfo framebuffer_info = {};
+      framebuffer_info.sType =
+      VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+      framebuffer_info.renderPass = render_pass;
+      framebuffer_info.attachmentCount = 1;
+      framebuffer_info.pAttachments = attachments;
+      framebuffer_info.width = swap_chain_extent.width;
+      framebuffer_info.height = swap_chain_extent.height;
+      framebuffer_info.layers = 1;
+
+      if (vkCreateFramebuffer(device, &framebuffer_info, nullptr,
+            &swap_chain_framebuffers[i]) != VK_SUCCESS) {
+        std::runtime_error("Failed to create framebuffer!");
+      }
     }
   }
 };
