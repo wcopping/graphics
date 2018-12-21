34156789203cc5e0a764638e8f6bfa268eab11c7
wyatt <wcopping@asu.edu>
2018-12-21 00:55:04 -0700

    create basic lighting model (Phong)

---
+++ learn-opengl/shaders/basic_lighting.fs
@@ -0,0 +1,33 @@
+#version 450 core
+out vec4 frag_color;
+
+in vec3 normal;  
+in vec3 frag_pos;  
+  
+uniform vec3 light_pos; 
+uniform vec3 view_pos; 
+uniform vec3 light_color;
+uniform vec3 object_color;
+
+void main()
+{
+  // ambient
+  float ambient_strength = 0.1;
+  vec3 ambient = ambient_strength * light_color;
+  
+  // diffuse 
+  vec3 norm = normalize(normal);
+  vec3 light_dir = normalize(light_pos - frag_pos);
+  float diff = max(dot(norm, light_dir), 0.0);
+  vec3 diffuse = diff * light_color;
+  
+  // specular
+  float specular_strength = 0.5;
+  vec3 view_dir = normalize(view_pos - frag_pos);
+  vec3 reflect_dir = reflect(-light_dir, norm);  
+  float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 32);
+  vec3 specular = specular_strength * spec * light_color;  
+   
+  vec3 result = (ambient + diffuse + specular) * object_color;
+  frag_color = vec4(result, 1.0);
+} 
--- learn-opengl/shaders/lamp.fs
+++ learn-opengl/shaders/lamp.fs
@@ -1,7 +1,7 @@
-#version 330 core
+#version 450 core
 out vec4 frag_color;
 
 void main()
 {
-    frag_color = vec4(1.0); // set alle 4 vector values to 1.0
+    frag_color = vec4(1.0); // set all 4 vector values to 1.0
 }
